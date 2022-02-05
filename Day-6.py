dfa56eed86ef3e49453cf9e04ef585965708c87d
pompypearson <mishrakash1701@gmail.com>
2022-02-05 14:24:13 +0530

    Day-6

---
+++ Day-6.py
@@ -0,0 +1,129 @@
+# Day-6 
+# Hurdle 1
+
+                # def turn_right():
+                #     turn_left()
+                #     turn_left()
+                #     turn_left()
+                # i = 1
+                # while i <= 6 :
+                #     move()
+                #     turn_left()
+                #     move()
+                #     turn_right()
+                #     move()
+                #     turn_right()
+                #     move()
+                #     turn_left()
+                #     i = i + 1
+# Hurdle 2
+
+
+                # def turn_up():
+                #     turn_left()
+                #     move()
+                    
+                # def turn_right():
+                #     turn_left()
+                #     turn_left()
+                #     turn_left()
+
+                # while at_goal() is False :
+                #     move()
+                #     turn_up()
+                #     turn_right()
+                #     move()
+                #     turn_right()
+                #     move()
+                #     turn_left()
+
+# Hurdle 3
+                # def turn_up():
+                #     turn_left()
+                #     move()
+                    
+                # def turn_right():
+                #     turn_left()
+                #     turn_left()
+                #     turn_left()
+
+                # def jump():
+                #     turn_up()
+                #     turn_right()
+                #     move()
+                #     turn_right()
+                #     move()
+                #     turn_left()
+                # while at_goal() is False :
+                #     if wall_in_front() is True :
+                #         jump()
+                #     else :
+                #         move()
+
+# Hurdle 4
+
+                # def turn_up():
+                #     turn_left()
+                #     move()
+                    
+                # def turn_right():
+                #     turn_left()
+                #     turn_left()
+                #     turn_left()
+
+                # def jump():
+                    
+                #     turn_up()
+                #     turn_right()     
+                
+                # def down():
+                #     turn_right()
+                    
+                #     i = 0 
+                #     while i < h:
+                    
+                #         move()
+                #         i = i + 1
+                #     turn_left()
+                        
+                        
+                
+                # while at_goal() is False :    
+                #     if wall_in_front() is True :
+                #         h = 0
+                #         while wall_in_front() is True:
+                #             jump()
+                #             h = h + 1
+                #         print(h)
+                #         move()
+                #         down()
+                        
+                        
+                #     elif wall_in_front() is False :
+                #         move()
+
+# Final Maze
+
+
+
+        
+
+    
+    
+            
+    
+        
+        
+    
+
+    
+  
+        
+
+   
+    
+    
+    
+
+    
+        
